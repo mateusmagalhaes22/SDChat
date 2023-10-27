dab6493ecebe7e59c5926c36cee1205ca07286cc
mateus <mateus.magalhaes@hotmail.com.br>
2023-10-27 11:12:22 -0300

    teste do back sem banco, ToDo: metodo Post_messages

--- aplication.py
+++ aplication.py
@@ -1,41 +1,62 @@
+import random
 from flask import Flask, jsonify, make_response, request
-import mysql.connector
 
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root",
-    database="sdchat"
-)
-
-cursor = db.cursor()
+class Room():
+    def __init__(self, id, nome, senha):
+        self.id = id
+        self.nome = nome
+        self.senha = senha
+        self.mensagens = []
 
 app = Flask(__name__)
 
-@app.route("/messages", methods=['GET'])
-def get_all():
-    cursor.execute("SELECT * FROM messages")
-    messages = cursor.fetchall()
-    data = list()
-    for message in messages:
+rooms = list()
+
+@app.route("/rooms", methods=['GET'])
+def get_all_rooms():
+    
+    data = []
+
+    for room in rooms:
         data.append({
-            'user': message[0],
-            'content': message[1],
-            'msgDate': message[2]
-        })
+            'id': room.id,
+            'nome': room.nome
+            })
     return make_response(jsonify(data))
 
-@app.route("/messages", methods=['POST'])
-def post_message():
+@app.route("/rooms", methods=['POST'])
+def post_room():
     requestBody = request.json
-    cursor.execute(f'''INSERT INTO messages 
-                   (user, content, msgDate) 
-                   VALUES ('{requestBody['user']}', 
-                   '{requestBody['content']}', 
-                   '{requestBody['msgDate']}');''')
+
+    ids = []
+    for i in range(0, 30):
+        ids.append(i)
+    for room in rooms:
+        ids.pop(room.id)
+    
+    if len(ids) == 0:
+        return make_response(jsonify({'error': "não é possivel criar mais salas"}))
+
+    roomId = random.randint(ids[0], ids[-1])
+
+    room = Room(roomId, requestBody['nome'], requestBody['senha'])
+
+    rooms.append(room)
+
     data = {
-        'user':requestBody['user'],
-        'content':requestBody['content'],
-        'msgDate':requestBody['msgDate']
+        'nome':requestBody['nome']
     }
+
+    return make_response(jsonify(data))
+
+@app.route("/rooms/<id>", methods=['GET'])
+def get_messages(id):
+    id = int(request.view_args['id'])
+    data = []
+    for room in rooms:
+        if room.id == id:
+            for msg in room.mensagens:
+                data.append(msg)
+            break
+
     return make_response(jsonify(data))
--- dockerfile
+++
@@ -1,5 +0,0 @@
-from mysql
-
-COPY ./dbConf/ /docker-entrypoint-initdb.d/
-
-EXPOSE 3306
