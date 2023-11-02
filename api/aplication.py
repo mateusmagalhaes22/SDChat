246b6be7e489289dd55b637b6f38f4a5db33ba72
mateus <mateus.magalhaes@hotmail.com.br>
2023-11-02 16:12:11 -0300

    messages

--- api/aplication.py
+++ api/aplication.py
@@ -8,88 +8,36 @@ class Mensagem():
         self.conteudo = conteudo
         self.data = datetime.now()
         
-
-class Room():
-    def __init__(self, id, nome, senha):
-        self.id = id
-        self.nome = nome
-        self.senha = senha
-        self.mensagens = []
-
 app = Flask(__name__)
 
-rooms = list()
+messages = list()
 
-@app.route("/rooms", methods=['GET'])
-def get_all_rooms():
-    
-    data = []
+@app.route("/messages", methods=['GET'])
+def get_messages():
 
-    for room in rooms:
+    data = list()
+    for msg in messages:
         data.append({
-            'id': room.id,
-            'nome': room.nome
-            })
-    return make_response(jsonify(data))
-
-@app.route("/rooms", methods=['POST'])
-def post_room():
-    requestBody = request.json
-
-    ids = []
-    for i in range(0, 30):
-        ids.append(i)
-    for room in rooms:
-        ids.pop(room.id)
-    
-    if len(ids) == 0:
-        return make_response(jsonify({'error': "não é possivel criar mais salas"}))
-
-    roomId = random.randint(ids[0], ids[-1])
-
-    room = Room(roomId, requestBody['nome'], requestBody['senha'])
-
-    rooms.append(room)
-
-    data = {
-        'nome':requestBody['nome']
-    }
+            "usuario": msg.usuario,
+            "conteudo": msg.conteudo,
+            "data": msg.data
+        })
 
     return make_response(jsonify(data))
 
-@app.route("/rooms/<id>", methods=['GET'])
-def get_messages(id):
-    id = int(request.view_args['id'])
-    data = []
-    for room in rooms:
-        if room.id == id:
-            for msg in room.mensagens:
-                data.append({
-                    'usuario': msg.usuario,
-                    'conteudo': msg.conteudo,
-                    'dataMsg': msg.data
-                })
-            break
-
-    return make_response(jsonify(data))
-
-@app.route("/rooms/<id>", methods=['POST'])
-def post_message(id):
+@app.route("/messages", methods=['POST'])
+def post_message():
     
     requestBody = request.json
-
-    id = int(request.view_args['id'])
 
     msg = Mensagem(requestBody['usuario'], requestBody['conteudo'])
+
+    messages.append(msg)
 
     data = {
         "usuario": requestBody['usuario'],
         "conteudo": requestBody['conteudo']
     }
-    for room in rooms:
-        if room.id == id:
-            room.mensagens.append(msg)
-            break
 
     return make_response(jsonify(data))
     
